ab912c9bed17b3700f70e26ada734e6ddee79a2c
pipo508 <j.verdini@alumno.um.edu.ar>
2023-08-20 02:35:27 -0300

    arreglo error de importacion de carpetas

--- Front/src/components/Card/Card.js
+++ Front/src/components/Card/Card.js
@@ -1,21 +1,30 @@
-import React from 'react'
-import Fondo from '../images/fondo.jpg'
-import './cards.css'
+import React from 'react';
+import fondo from "../images/fondo.jpg"; 
+import "./Cards.css";
+
 export const Card = (props) => {
+  const cardStyle = {
+    width: '18rem',
+    backgroundImage: `url(${fondo})`, 
+    backgroundSize: 'cover'
+  };
+
   return (
-<div>
-    <div className= "row">
-      <div className='col'>
-        <div className="card cards-text" style={{width: '18rem', backgroundImage: `url(${Fondo})`, backgroundSize: 'cover'}}>
-          <img src={require(`../images/${props.imagen}`)} className="card-img-top" alt=".." />
+    <div>
+      <div className="row">
+        <div className='col'>
+          <div className="card cards-text" style={cardStyle}>
+            <img src={require(`../images/${props.imagen}`)} className="card-img-top" alt=".." />
             <div className="card-body">
-              <h1 className='titulo' style={{color: 'rgb(223, 164, 0)', fontSize: '22px'}}>{ props.titulo }</h1>
-              <p className="texto" style={{color: 'rgb(223, 164, 0)', fontSize: '15px'}}>{ props.texto }</p>
-              <a href="#" class="btn btn-primary">+Info</a>
+              <h1 className='titulo' style={{ color: 'rgb(223, 164, 0)', fontSize: '22px' }}>{props.titulo}</h1>
+              <p className="texto" style={{ color: 'rgb(223, 164, 0)', fontSize: '15px' }}>{props.texto}</p>
+              <a href="/algo" className="btn btn-primary">+Info</a> 
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
+  );
+};
+
+export default Card;
--- Front/src/pages/Nuestras_cervezas.js
+++ Front/src/pages/Nuestras_cervezas.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar } from "../components/Navbar/Navbar";
-import { Card } from "../components/Card/Card";
+import {Card} from "../components/Card/Card";
 
 export const NuestrasCervezas = () => {
   return (
